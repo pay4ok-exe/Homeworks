e2a738fd10995cd7405686593c112868928979c4
Akim Ospan <ospanakim05@gmail.com>
2024-08-29 03:14:52 +0500

    Added Project

--- NFactorial/Homework9/main.js
+++ NFactorial/Homework9/main.js
@@ -1,5 +1,9 @@
 async function getTopStories(){
     const response = await fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=j8tv9eNtTyTcIS02ZJu5uwTec760vGeC');
+    if(!response.ok && response.status === '401'){
+        console.log('Error');   
+    }
+
     const resJson = await response.json();
     console.log(resJson)
 
